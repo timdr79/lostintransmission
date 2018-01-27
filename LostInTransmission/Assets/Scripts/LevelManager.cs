4501cdc242851021587b933475cc7e22ca765766
MT_CENT <mtc9417@gmail.com>
2018-01-27 15:40:42 -0500

    Menu system

---
+++ LostInTransmission/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	public void StartGameBtn(string Main){
+		SceneManager.LoadScene ("Main");
+	}
+	public void GameoverScreen(string GameOver){
+		SceneManager.LoadScene ("GameOver");
+	}
+	public void VictoryScreen(string Victory){
+		SceneManager.LoadScene ("Victory");
+	}
+	public void MainMenuBtn(string Start){
+		SceneManager.LoadScene ("Start");
+	}
+}
+
