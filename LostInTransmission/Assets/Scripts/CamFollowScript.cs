7c1003bce0c91795a68cb1629b6b526864b7cbd2
Justin Cockburn <justin.cockburn@gramercytech.com>
2018-01-27 15:06:19 -0500

    Adding in Wave Prefab with motion functionality

---
+++ LostInTransmission/Assets/Scripts/CamFollowScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollowScript : MonoBehaviour {
+
+	// Store a refrence of the ship's game object
+	public GameObject theCharacter;
+
+	// store a speed at which we want to follow the ship
+	public float followSpeed;
+
+
+	// Fixed update is called automatically by MonoBehaviour
+	void FixedUpdate () {
+
+		// store the distance on every fixed update frame between the camera parent / transform and the ship's transform position.
+		float superDistance = Vector2.Distance (transform.position, theCharacter.transform.position);
+
+		// Assign a new vector 3 to the transform's position that is calculated by the lerp function. 
+		// The lerp function takes 3 arguments a: where are we learping from. b: where are we learping to. c: where on the scale between the 2 we want to be
+		transform.position = Vector3.Lerp (transform.position, theCharacter.transform.position,followSpeed* superDistance * Time.deltaTime);
+	}
+}
