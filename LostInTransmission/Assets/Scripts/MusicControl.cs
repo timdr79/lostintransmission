4893f24a580297f5df85135892456a9964ab01c7
Justin Cockburn <justin.cockburn@gramercytech.com>
2018-01-27 19:23:22 -0500

    Adding music prefabs and trigger functionality

---
+++ LostInTransmission/Assets/Scripts/MusicControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+//		Debug.Log ("Object entered the trigger");
+
+	}
+
+	void OnTriggerStay(Collider other) 
+	{
+//		Debug.Log ("Object in the trigger");
+
+	}
+
+	void OnTriggerExit(Collider sother)
+	{
+//		Debug.Log ("Object exited the trigger");
+	
+	}
+
+}
