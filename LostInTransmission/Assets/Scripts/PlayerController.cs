414f0e74d176de58ce6b5e7699c953aa04c62ce4
Timothy Robinson <timothyrobinson@Timothys-MacBook-Air.local>
2018-01-27 23:58:07 -0500

    Updated Cat Prefab and tied Defeat Scene to zero health

--- LostInTransmission/Assets/Scripts/PlayerController.cs
+++ LostInTransmission/Assets/Scripts/PlayerController.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
 
 	public Slider healthBar;
 	public float health;
 	public float dropRate;
+	public Text lowHealthText;
 
 	void Start() {
 
@@ -20,14 +22,13 @@ public class PlayerController : MonoBehaviour {
 
 		// Health gradually decreases over time of game
 		health += Time.deltaTime * dropRate;
-
 		healthBar.value = health;
-		/*if (Input.GetKeyDown(KeyCode.Space)) {
-
-			health -= Time.deltaTime * 1.0f;
-			healthBar.value = health;
-	
-		}*/
+		if (health <= 0.2) {
+			lowHealthText.enabled = true;
+		}
+		if (health <= 0) {
+			SceneManager.LoadScene("Defeat Scene");
+		}
 	}
 
 // if we collide with an object and its not the same as the last object we collided with, take damage and update the health bar. 
@@ -45,10 +46,6 @@ public class PlayerController : MonoBehaviour {
 			other.transform.GetChild(1).gameObject.SetActive(true);
 			other.transform.GetChild(0).gameObject.SetActive(false);
 
-		}
-
-		if (health <= 0) {
-		//	Debug.LogError ("Stop");
 		}
 	}
 }
